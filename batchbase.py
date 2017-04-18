f1e067fdaf5ac8f09c39bec0579e4ef191cceafd
Max von Hippel <maxvonhippel1996@gmail.com>
2017-04-18 15:32:59 -0700

    began setting up yaml parsing

--- batchbase.py
+++ batchbase.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
+import sys
 import yaml
-import fileinput
 
-def process(line):
-	print(line)
+# read the yaml file
+def parse_yaml(yaml):
+	print('hello')
 
-for line in fileinput.input():
-    process(line)
+# handle command line args
+def main(argv):
+	with open(argv[0]) as stream:
+		try:
+			parse_yaml(yaml.load(stream))
+			return 0
+		except yaml.YAMLError as exc:
+			print(exc)
+			return 1
+
+if __name__ == "__main__":
+	sys.exit(main(sys.argv[1:]))
