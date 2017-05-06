6b961ff86811ed39e66834d46d16dbb2a7fd1a86
Max von Hippel <maxvonhippel1996@gmail.com>
2017-05-05 17:50:40 -0700

    more progress on basic infrastructure

--- batchbase.py
+++ batchbase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys
 import yaml
+import glob
+import subprocess
 
 num = 0
 
@@ -18,11 +20,27 @@ class batch:
 		self.output_dir = output_dir
 		self.additional_settings = additional_settings
 		self.codes = codes
-	# def run:
-		# run the instructions for this batch
+	# run the instructions for this batch
+	def run(self):
+		# first, find the files in the batch
+		files = glob.glob(self.input_dir)
+		# if files empty, log it
+		if not files:
+			# later need to work out logging in this case
+			print("no input files found matching path: " + self.input_dir + " for batch: " + self.number)
+		# if files not empty, start running each file
+		# http://stackoverflow.com/a/4256153/1586231
+		# need to figure out what use cases this tool will have
+		# (which modules to support)
+		# and work on command formatting accordingly
+		# with appropriate flags
+		else for file in files:
+			process = subprocess.Popen('whatever','commands','we','are','using','tbd', stdout=subprocess.PIPE)
+			output, error = process.communicate()
 
 # read the yaml file
 def parse_yaml(yaml):
+	batches = []
 	for yaml_batch in yaml:
 		this_batch = batch(yaml[yaml_batch]['yaml'],\
 			yaml[yaml_batch]['input_dir'],\
@@ -36,6 +54,8 @@ def parse_yaml(yaml):
 			print('setting: ' + setting)
 		for code in yaml[yaml_batch]['codes']:
 			print('code: ' + code)
+		batches.append(this_batch)
+	return batches
 
 # handle command line args
 def main(argv):
